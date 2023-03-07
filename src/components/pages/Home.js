0b82fe34040c5ac003376318646df3ebc7c2a51f
unknown <mescamilla1114@gmail.com>
2023-03-06 23:35:05 -0600

    updated about page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -19,45 +19,33 @@ const styles = {
 function Home() {
   return (
     <section style={styles.sectionStyles} className="section">
-      <h2>Lorem Ipsum Dolor Sit Amet</h2>
+      <h2>About me</h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas molestias excepturi sint occaecati cupiditate non provident,
-        similique sunt in culpa qui officia deserunt mollitia animi, id est
-        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-        cumque nihil impedit quo minus id quod maxime placeat facere possimus,
-        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
-        quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
-        ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
-        earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-        voluptatibus maiores alias consequatur aut perferendis doloribus
-        asperiores repellat.
+      Behind the scenes, I live, love, and keep busy in Austin. It’s not just the city my family represents fiercely, but also our hometown. I’ve spent my life in this place - raised by hard working migrant parents, I found adventure, worked long days and nights downtown, pursued business ventures, and established my own family. Along the way, fulfillment has followed from connection with people of diverse backgrounds and overlapping their stories with mine.
+
+
+
+In 2021, my story changed when my son, Salem, was born and spent 70 days in the NICU at St. David’s Main, where his life was truly saved. It was a time defined simultaneously by the all-consuming joy of new parenthood and the heartbreak of medical unknowns. This is an experience that will always remain a part of me. I now participate in parent panels and mentoring services through Hand to Hold, a nonprofit that provides support and resources to NICU families.
+
+
+
+I’m a father, a husband, a son, a brother, a friend, and a citizen of the world. I’m a seeker of the small but magnificent things, and a champion of the bigger concepts aimed at today’s most pressing problems.
+
+
+
+You can find me brainstorming and building my next project for good, losing my voice at an Austin FC soccer game, chasing our endlessly pesky toddler, cheering on my F1 team, and looking forward to that ever-elusive next quiet moment.
+
+
+
+Let’s collaborate.
+
+
+
+Cheers,
+
+
+
+Michael
       </p>
     </section>
   );
